22947b1b4dcee99a7037a0348d3d4c35de2fc689
CRASH3000 <crash-vd@yandex.ru>
2025-10-19 19:54:11 +0300

    Лабораторная работа №1 - Выполнен этап 1: Генерация сервиса на Java - Выполнен этап 2: Создание контейнера Docker с БД PostgreSQL (работоспособность контейнара проверена).

---
+++ src/main/java/ru/rksp/Vikhrov/VikhrovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Vikhrov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikhrovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikhrovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Vikhrov/VikhrovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Vikhrov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikhrovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
